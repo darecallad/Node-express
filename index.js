6aec6d0f6c6e0c1a9e5ad61622118fef078b312d
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-18 10:38:31 -0700

    healing delete

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("the course with given ID was not found");
+  if (!course)
+    return res.status(404).send("the course with given ID was not found");
   res.send(course);
   //   res.send(req.params.id);
 
@@ -34,8 +35,7 @@ app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
   if (error) {
     // 400 bad request
-    res.status(400).send(error.details[0].message);
-    return;
+    return res.status(400).send(error.details[0].message);
   }
 
   const course = {
@@ -58,14 +58,15 @@ app.put("/api/courses/:id", (req, res) => {
   // first lool up the course
   // if not existing, return 404
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("the course with given ID was not found");
+  if (!course) {
+    return res.status(404).send("the course with given ID was not found");
+  }
   // validate if incalid, return 400 -bad req
 
   const { error } = validateCourse(req.body);
   if (error) {
     // 400 bad request
-    res.status(400).send(error.details[0].message);
-    return;
+    return res.status(400).send(error.details[0].message);
   }
   // update course
   // return the updated course
@@ -80,3 +81,16 @@ function validateCourse(course) {
 
   return Joi.validate(course, schema);
 }
+
+app.delete("api/courses/:id", (req, res) => {
+  //look up the course
+  // not existing, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("the course with given ID was not found");
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  // Return the same course
+  res.send(course);
+});
